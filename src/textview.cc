db1a40b6a2f3acd1ac5596f0688c2daf468f8b39
Zaid Abdulameer <zabdulam33r@gmail.com>
2024-07-23 11:48:39 -0400

    Finish output without operator overloading

--- src/textview.cc
+++ src/textview.cc
@@ -10,9 +10,36 @@ void TextView::notify() {
         for (int x = 0; x < BOARD_MAX_WIDTH; ++x) {
             const Piece *piece = subject->getItem(x, y);
             if (piece == nullptr) {
-                std::cout << (dark ? "_" : " ") << std::endl;
+                std::cout << (dark ? '_' : ' ') << std::endl;
             } else {
-                std::cout << *piece << std::endl;
+                char out;
+                switch (piece->getType())
+                {
+                case Type::KING:
+                    out = 'K';
+                    break;
+                case Type::QUEEN:
+                    out = 'Q';
+                    break;
+                case Type::BISHOP:
+                    out = 'B';
+                    break;
+                case Type::ROOK:
+                    out = 'R';
+                    break;
+                case Type::KNIGHT:
+                    out = 'N';
+                    break;
+                case Type::PAWN:
+                    out = 'P';
+                    break;
+                default:
+                    break;
+                }
+                if (piece->getSide() == Colour::BLACK) {
+                    out += 'a' - 'A'; // convert to lowercase 
+                }
+                std::cout << out << std::endl;
             }
             dark = !dark; // next square will be the opposite of current
         }
