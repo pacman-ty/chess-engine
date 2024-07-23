17b148dea96ac5a6844617b0f3b69e8ea790eccb
pacman-ty <talha.yildirim720@gmail.com>
2024-07-22 20:18:33 -0400

    finished piece.cc

--- src/piece.cc
+++ src/piece.cc
@@ -2,3 +2,22 @@
 
 Piece::Piece(Type t) : t{t} {}
 
+Piece::getSide() {
+    return side;
+}
+
+Piece::getType() {
+    return t;
+}
+
+Piece::getPos() {
+    return currPosition;
+}
+
+Piece::setSide(Colour c) {
+    colour = c;
+}
+
+Piece::setPos(Position p) {
+    currPosition = p;
+}
