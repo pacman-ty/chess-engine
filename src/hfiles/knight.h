21c5178b6a3ce68b3eb41acdb1bfcc49ff046c76
pacman-ty <talha.yildirim720@gmail.com>
2024-07-23 17:15:00 -0400

    finished knight.cc for real this time :skull:

--- src/hfiles/knight.h
+++ src/hfiles/knight.h
@@ -7,6 +7,7 @@
 
 class Knight : public Piece {
 public:
+    Knight();
     // return list of possible moves (does not mean that moves are legal)
     virtual std::vector<Move> getPossibleMoves(const Board::BoardType &) const override;
     // return list of attacking moves (does not mean that moves are legal)
--- src/knight.cc
+++ src/knight.cc
@@ -1,8 +1,123 @@
 #include "knight.h"
-Knight::Knight() : Piece{Type::Knight} {}
+Knight::Knight() : Piece{Type::KNIGHT} {}
 
-// check each direction the rook can move not including captures and return a vector containing the moves 
-std::vector<Move> Rook::getPossibleMoves(const Board::BoardType & board) const {
+// check each direction the knight can move not including captures and return a vector containing the moves 
+std::vector<Move> Knight::getPossibleMoves(const Board::BoardType & board) const {
     std::vector<Move> output;
     int curX = currPosition.getX();
     int curY = currPosition.getY();
+
+    if (curX + 2 < BOARD_MAX_WIDTH && curY + 1 < BOARD_MAX_HEIGHT) {
+        if (board[curX + 2][curY + 1] == nullptr) {
+            output.emplace_back(currPosition, Position(curX + 2, curY + 1), this, nullptr);
+        }
+    }
+    if (curX + 2 < BOARD_MAX_WIDTH && curY - 1 >= BOARD_MIN_HEIGHT) {
+        if (board[curX + 2][curY - 1] == nullptr) {
+            output.emplace_back(currPosition, Position(curX + 2, curY - 1), this, nullptr);
+        }
+    }
+
+    if (curX - 2 >= BOARD_MIN_WIDTH && curY + 1 < BOARD_MAX_HEIGHT) {
+        if (board[curX - 2][curY + 1] == nullptr) {
+            output.emplace_back(currPosition, Position(curX - 2, curY + 1), this, nullptr);
+        }
+    }
+    if (curX - 2 >= BOARD_MIN_WIDTH && curY - 1 >= BOARD_MIN_HEIGHT) {
+        if (board[curX - 2][curY - 1] == nullptr) {
+            output.emplace_back(currPosition, Position(curX - 2, curY - 1), this, nullptr);
+        }
+    }
+
+    if (curX + 1 < BOARD_MAX_WIDTH && curY + 2 < BOARD_MAX_HEIGHT) {
+        if (board[curX + 1][curY + 2] == nullptr) {
+            output.emplace_back(currPosition, Position(curX + 1, curY + 2), this, nullptr);
+        }
+    }
+    if (curX + 1 < BOARD_MAX_WIDTH && curY - 2 >= BOARD_MIN_HEIGHT) {
+        if (board[curX + 1][curY - 2] == nullptr) {
+            output.emplace_back(currPosition, Position(curX + 1, curY - 2), this, nullptr);
+        }
+    }
+
+    if (curX - 1 >= BOARD_MIN_WIDTH && curY + 2 < BOARD_MAX_HEIGHT) {
+        if (board[curX - 1][curY + 2] == nullptr) {
+            output.emplace_back(currPosition, Position(curX - 1, curY + 2), this, nullptr);
+        }
+    }
+    if (curX - 1 >= BOARD_MIN_WIDTH && curY - 2 >= BOARD_MAX_HEIGHT) {
+        if (board[curX - 1][curY - 2] == nullptr) {
+            output.emplace_back(currPosition, Position(curX - 1, curY - 2), this, nullptr);
+        }
+    }
+
+    return output;
+}
+
+std::vector<Move> Knight::getPossibleMoves(const Board::BoardType & board) const {
+    std::vector<Move> output;
+    int curX = currPosition.getX();
+    int curY = currPosition.getY();
+
+    if (curX + 2 < BOARD_MAX_WIDTH && curY + 1 < BOARD_MAX_HEIGHT) {
+        if (board[curX + 2][curY + 1] != nullptr ) {
+            if (board[curX + 2][curY + 1]->getSide() != this->getSide()) {    
+                output.emplace_back(currPosition, Position(curX + 2, curY + 1), this, nullptr);
+            }
+        }
+    }
+    if (curX + 2 < BOARD_MAX_WIDTH && curY - 1 >= BOARD_MIN_HEIGHT) {
+        if (board[curX + 2][curY - 1] != nullptr) {
+            if (board[curX + 2][curY - 1]->getSide() != this->getSide()) {    
+                output.emplace_back(currPosition, Position(curX + 2, curY - 1), this, nullptr);
+            }
+        }
+    }
+
+    if (curX - 2 >= BOARD_MIN_WIDTH && curY + 1 < BOARD_MAX_HEIGHT) {
+        if (board[curX - 2][curY + 1] != nullptr) {
+            if (board[curX - 2][curY + 1]->getSide() != this->getSide()) {    
+                output.emplace_back(currPosition, Position(curX - 2, curY + 1), this, nullptr);
+            }
+        }
+    }
+    if (curX - 2 >= BOARD_MIN_WIDTH && curY - 1 >= BOARD_MIN_HEIGHT) {
+        if (board[curX - 2][curY - 1] != nullptr) {
+            if (board[curX - 2][curY - 1]->getSide() != this->getSide()) {    
+                output.emplace_back(currPosition, Position(curX - 2, curY - 1), this, nullptr);
+            }
+        }
+    }
+
+    if (curX + 1 < BOARD_MAX_WIDTH && curY + 2 < BOARD_MAX_HEIGHT) {
+        if (board[curX + 1][curY + 2] != nullptr) {
+            if (board[curX + 1][curY + 2]->getSide() != this->getSide()) {    
+                output.emplace_back(currPosition, Position(curX + 1, curY + 2), this, nullptr);
+            }
+        }
+    }
+    if (curX + 1 < BOARD_MAX_WIDTH && curY - 2 >= BOARD_MIN_HEIGHT) {
+        if (board[curX + 1][curY - 2] != nullptr) {
+            if (board[curX + 1][curY - 2]->getSide() != this->getSide()) {    
+                output.emplace_back(currPosition, Position(curX + 1, curY - 2), this, nullptr);
+            }
+        }
+    }
+
+    if (curX - 1 >= BOARD_MIN_WIDTH && curY + 2 < BOARD_MAX_HEIGHT) {
+        if (board[curX - 1][curY + 2] != nullptr) {
+            if (board[curX - 1][curY + 2]->getSide() != this->getSide()) {    
+                output.emplace_back(currPosition, Position(curX - 1, curY + 2), this, nullptr);
+            }
+        }
+    }
+    if (curX - 1 >= BOARD_MIN_WIDTH && curY - 2 >= BOARD_MAX_HEIGHT) {
+        if (board[curX - 1][curY - 2] != nullptr) {
+            if (board[curX - 1][curY - 2]->getSide() != this->getSide()) {    
+                output.emplace_back(currPosition, Position(curX - 1, curY - 2), this, nullptr);
+            }
+        }
+    }
+
+    return output;
+}
