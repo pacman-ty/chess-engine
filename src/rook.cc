6900af69b6d1b48e0f4fd3637446c41adbbf0878
Jason <cpjason1234@gmail.com>
2024-07-23 11:25:51 -0400

    Fixed endl issue

--- src/rook.cc
+++ src/rook.cc
@@ -90,8 +90,8 @@ std::vector<Move> Rook::getPossibleCaptures(const Board::BoardType & board) cons
 
 std::ostream& operator<<(std::ostream& out, Rook & r) {
     switch (r.getSide()) {
-        case Colour::BLACK: out << ROOK_SYM << std::endl;
-        case Colour::WHITE: out << ROOK_SYM + 'A' - 'a' << std::endl;
+        case Colour::BLACK: out << ROOK_SYM;
+        case Colour::WHITE: out << ROOK_SYM + 'A' - 'a';
         default:
             std::cerr << "Bad Side" << std::endl;
     }
