95c27c080acb62bd35ada20fdc07c008fbe11233
jkhhon <jkhhon@uwaterloo.ca>
2024-07-26 16:00:10 -0400

    final

--- src/includes/move.h
+++ src/includes/move.h
@@ -12,12 +12,8 @@ private:
     const Piece* target;
     const Piece* capture; 
 public:
-<<<<<<< HEAD
     Move() = default;
-    Move(Position oldPos, Position newPos, const Piece *target, Piece *capture);
-=======
     Move(Position oldPos, Position newPos, const Piece *target, const Piece *capture);
->>>>>>> 7e486e5fa6fa83927de3a84903739f2446a56554
     Position getOldPosition() const;
     Position getNewPosition() const;
     const Piece* getCapture() const;
